a7eed22bc5f8482f032d0ef8b8fb705890291137
workani <tomdanil68@gmail.com>
2024-08-04 21:57:23 +0200

    fixed couple of varibale overshading issues and some other small fixes

--- vocab.c
+++ vocab.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <unistd.h>
 #include <time.h>
 
 
-//define clear function for windows and other systems separetly
+// ensure cross platform compability by defining clear function and import diffrent libraries for windows and diffrent systems
 #ifdef _WIN32
+#include <windows.h>
 #define CLEAR  "cls"
 #else
+#include <unistd.h>
 #define CLEAR "clear"
 #endif
 
@@ -31,6 +32,10 @@
  char **source_vocab = NULL;
  char **target_vocab = NULL;
 
+// arrays for storing user's correct and incorrect answers
+ char **correct_input = NULL;
+ char **incorrect_input = NULL;
+
 //count amount of lines
 int lines_count = 0;
 
@@ -38,7 +43,7 @@ int lines_count = 0;
 int idx = 0;
 int word_count = 0;
 int correct_answers = 0;
-int incorrect_answers = 0; 
+int incorrect_answers = 0;
 int score = 0;
 
 // all prototypes for script's functions
@@ -47,21 +52,54 @@ void get_filename(char *fname);
 void allocate_mem(char **dictionary, int n);
 void free_mem(void);
 void populate_arrs(FILE *fptr);
-void get_input(char *user_input, int idx);
+void get_input(char *user_input, int index);
 void print_results(void);
 void print_detailed_results(void);
-void correct_answer(void);
-void incorrect_answer(char *input, int idx);
+void correct_answer(char *input);
+void incorrect_answer(char *input, int index);
 
-// functoins to get random index for vocab arrays
+// functoins to get random index for vocab arrays and pointer to array for storing them
+int *indices;
 void swap(int *a, int *b);
-void shuffle(int *indices);
+void shuffle(int *arr);
+
+
+
+// custom strcasecmp and sleep function to insure ensure compatibility across different platforms.
+typedef unsigned char u_char;
+
+
+int str_casecmp(const char *s1, const char *s2) {
+    while (*s1 && *s2) {
+        int c1 = tolower((unsigned char)*s1);
+        int c2 = tolower((unsigned char)*s2);
+
+        if (c1 != c2) {
+            return c1 - c2;
+        }
+
+        s1++;
+        s2++;
+    }
+
+    return tolower((unsigned char)*s1) - tolower((unsigned char)*s2);
+}
+
+
+void custom_sleep(int seconds)
+{
+    #ifdef _WIN32
+        Sleep(seconds * 1000);
+    #else
+        sleep(seconds);
+    #endif
+}
 
 
 
 int main(void)
 {
-    srand(time(NULL)); //initalize random generator for future use 
+    srand(time(NULL)); //initalize random generator for future use
 
     char *filename = malloc(256 * sizeof(char));
     get_filename(filename);
@@ -80,26 +118,29 @@ int main(void)
     {
         system(CLEAR);
         printf("%sFile loaded succesfully!\n", GRN);
-        sleep(1);
+        custom_sleep(1);
         system(CLEAR);
     }
 
     lines_count = count_lines(f);
 
-    
-    int indices[lines_count];
+    source_vocab = malloc(lines_count * sizeof(char*));
+    target_vocab = malloc(lines_count * sizeof(char*));
 
-    for(int i = 0; i < lines_count; i++) // initalize an array of indices for random interation thru vocabulary arrays 
+    correct_input = malloc(lines_count * sizeof(char*));
+    incorrect_input = malloc(lines_count * sizeof(char*));
+
+
+    indices = malloc(lines_count *sizeof(int));
+
+    for(int i = 0; i < lines_count; i++) // initalize an array of indices for random interation thru vocabulary arrays
     {
         indices[i] = i;
     }
 
     shuffle(indices);
 
-    source_vocab = malloc(lines_count * sizeof(char*));
-    target_vocab = malloc(lines_count * sizeof(char*));
-
-    if(source_vocab == NULL || target_vocab == NULL)
+    if(source_vocab == NULL || target_vocab == NULL  || indices == NULL)
     {
         printf("%sMemory Error. Exiting..", RED);
         free_mem();
@@ -109,6 +150,8 @@ int main(void)
     {
         allocate_mem(source_vocab, lines_count);
         allocate_mem(target_vocab, lines_count);
+        allocate_mem(correct_input, lines_count);
+        allocate_mem(incorrect_input, lines_count);
     }
 
     // populating source and target arrays with content of user's csv file
@@ -116,25 +159,24 @@ int main(void)
 
     char input[STRING_SIZE];
 
-   
-    // loop until the end of the loaded vocabulary or until the user prints exit
-    while(word_count < lines_count && strcasecmp(input, "exit") != 0)
-    {
-        int idx = indices[word_count];
 
-        char input[STRING_SIZE];
+    // loop until the end of the loaded vocabulary or until the user prints exit
+    while(word_count < lines_count && str_casecmp(input, "exit") != 0)
+    {
+        idx = indices[word_count];
+
         get_input(input, idx);
 
-        if(strcasecmp(input, target_vocab[idx]) == 0)
+        if(str_casecmp(input, target_vocab[idx]) == 0)
         {
-            correct_answer();
+            correct_answer(input);
         }
         else
-        { 
+        {
             incorrect_answer(input, idx);
         }
 
-        if(strcasecmp(input, "exit") == 0)
+        if(str_casecmp(input, "exit") == 0)
         {
             break;
         }
@@ -142,18 +184,18 @@ int main(void)
         system(CLEAR);
         word_count++;
     }
-    
+
     //print out results
     print_results();
 
-    // clean up memory 
+    // clean up memory
     free_mem();
 }
 
 int count_lines(FILE *f)
 {
     char c;
-    int lines_count = 1;
+    lines_count = 1;
 
    // Reading file until the end and incrementing lines_count with each new line
     do
@@ -198,6 +240,8 @@ void free_mem(void)
 {
     free(source_vocab);
     free(target_vocab);
+    free(incorrect_input);
+    free(indices);
 }
 
 
@@ -217,22 +261,20 @@ void populate_arrs(FILE *fptr)
         {
             size_t len = strlen(token);
             strncpy(source_vocab[count], token, len);
-            //source_vocab[count][len] = '\n';
         }
 
         token = strtok(NULL, ";");
-       
+
         if(token != NULL)
         {
             size_t len = strlen(token);
             strncpy(target_vocab[count], token, len);
-            //target_vocab[count][len] = '\n';
         }
         count++;
     }
 }
 
-void get_input(char *user_input, int idx)
+void get_input(char *user_input, int index)
 {
     system(CLEAR);
     printf("%sTotal: %i/%i\n", MAG, word_count, lines_count);
@@ -251,7 +293,7 @@ void print_results(void)
     printf("%sIncorrect answers: %i\n", RED, incorrect_answers);
     printf("%s+-----------------------------------------------------------------------------+\n", CYN);
     printf("%sDo you want to see detailed results of your session? (y or n): ", WHT);
-    
+
     // get character input from user and convert it to lowercase
     scanf(" %c", &choice);
     choice = tolower(choice);
@@ -272,34 +314,56 @@ void print_results(void)
 
 void print_detailed_results(void)
 {
-    ;
+    system(CLEAR);
+
+    // print out all correct and incorrect answers if they exist
+    if(correct_answers > 0 && correct_input != NULL)
+    {
+        printf("%sCorrect answers:\n", GRN);
+        for(int i = 0; i < correct_answers; i++)
+        {
+            printf("%s\n", correct_input[i]);
+        }
+    }
+
+
+    if(incorrect_answers > 0)
+    {
+        printf("%sIncorrect answers:\n", RED);
+        for(int j = 0; j < incorrect_answers; j++)
+        {
+            printf("%s\n", incorrect_input[j]);
+        }
+    }
 }
 
-void correct_answer(void)
+void correct_answer(char *input)
 {
+    correct_input[word_count] = input;
     correct_answers++;
     score++;
     system(CLEAR);
     printf("%sCorrect!\n", GRN);
-    sleep(1);
+    custom_sleep(1);
 }
 
-void incorrect_answer(char *input, int idx)
+void incorrect_answer(char *input, int index)
 {
     system(CLEAR);
 
     // checking if user is exiting from the program; if not, incrementing incorrect answer count.
-    if(strcasecmp(input, "exit") != 0) 
+    if(str_casecmp(input, "exit") != 0)
     {
+        incorrect_input[word_count] = input;
         incorrect_answers++;
         printf("%sIncorrect :(\n", RED);
     }
 
-    while(strcasecmp(input, target_vocab[idx]) != 0 && strcasecmp(input, "exit") != 0)
+    while(str_casecmp(input, target_vocab[idx]) != 0 && str_casecmp(input, "exit") != 0)
     {
         get_input(input, idx);
     }
-    sleep(1);
+    custom_sleep(1);
 }
 
 void swap(int *a, int *b)
@@ -309,7 +373,7 @@ void swap(int *a, int *b)
     *b = tmp;
 }
 
-void shuffle(int *indices)
+void shuffle(int *arr)
 {
     for(int i = lines_count - 1; i > 0; i--)
     {
